241837ba309f30baa0bd5bf57d61c11e0b96b2d9
Pallavi Patidar <PallaviP@CCMT.local>
2018-02-10 23:14:00 +0530

    ...

--- runner.py
+++ runner.py
@@ -4,12 +4,15 @@ from docx.shared import Inches
 import requests
 import os
 import sys
+import re
 
 
-#r  = requests.get("https://www.reliablesoft.net/top-10-search-engines-in-the-world/")
-url = "https://www.reliablesoft.net/top-10-search-engines-in-the-world/"
-r  = requests.get(url)
+index = 0
+limit = int(input("Maximum number of url's you want to display:"))
+
+r  = requests.get("https://www.reliablesoft.net/top-10-search-engines-in-the-world/")
 data = r.text
+
 
 soup = BeautifulSoup(data, "lxml")
 document = Document()
@@ -17,25 +20,33 @@ document.add_heading('List of urls')
 
 yes = {'yes','y'}
 no = {'no','n'}
-#input('Would you like to download Docx file Please respond with yes or no:')
 
 choice = input('Would you like to download Docx file Please respond with yes or no:').lower()
 if choice in yes:
         try:
                 for link in soup.find_all('a'):
-                        # If docx file has some error please uncomment the below print line
-                        # print(link.get('href'))
+                        pattern = (r'http[s]?://(?:[a-z]*\.(google|bing)\.)+')
+                        if re.match(pattern, link.get('href')) is not None:
 
-                        # Adding data to docx file
-                        document.add_paragraph(link.get('href'))
+                                # Adding data to docx file
+                                document.add_paragraph(link.get('href'))
+                                index += 1
+                                if index == limit:
+                                        break
                 document.save('scrap.docx')
+                print('Please check the docx file with name scrape.docx')
 
         except OSError:
                 pass
 
 elif choice in no:
-        for link in soup.find_all('a'):
-                print(link.get('href'))
+                for link in soup.find_all('a'):
+                        #print(link.get('href'))
+                        pattern = (r'http[s]?://(?:[a-z]*\.(google|bing)\.)+')
+                        if re.match(pattern, link.get('href')) is not None:
+                                print(link.get('href'))
+                                index += 1
+                                if index == limit:
+                                        break
 else:
-   sys.stdout.write("Please respond with 'yes' or 'no'")
-
+   sys.stdout.write("Please respond with 'yes' or 'no' after running the program again")
